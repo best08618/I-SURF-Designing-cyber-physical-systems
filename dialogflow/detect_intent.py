ed9ca8595b2a4488e3a12e9e84f484b455d9e4ce
swzoey <tmddnnim2da@gmail.com>
2018-11-13 18:50:59 -0800

    change detect_intent.py

--- dialogflow/detect_intent.py
+++ dialogflow/detect_intent.py
@@ -1,18 +1,40 @@
-import dialogflow
+#!/usr/bin/env python
+# coding: utf-8
+
+
 import uuid
+import argparse
+
+
+
+def explicit():
+    from google.cloud import storage
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json('/Users/zoey/Desktop/dialogflow/seungwoojeong-6d73c1832690 copy.json')
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
+
+explicit()
+
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
     """Returns the result of detect intent with texts as inputs.
-        Using the same `session_id` between requests allows continuation
-        of the conversation."""
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
     file = open('output.txt','w')
-    
+
     import dialogflow_v2 as dialogflow
-    session_client = dialogflow.SessionsClient()
-    
+    session_client = dialogflow.SessionsClient.from_service_account_json('/Users/zoey/Desktop/dialogflow/seungwoojeong-6d73c1832690 copy.json')
+
     session = session_client.session_path(project_id, session_id)
     print('Session path: {}\n'.format(session))
-    
+
     for text in texts:
         text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
         query_input = dialogflow.types.QueryInput(text=text_input)
@@ -20,17 +42,16 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
         
         file.write('Query text: {}'.format(response.query_result.query_text))
         file.write('Detected intent: {} (confidence: {})\n'.format(response.query_result.intent.display_name, response.query_result.intent_detection_confidence))
-        
+
         print('=' * 20)
         print('Query text: {}'.format(response.query_result.query_text))
         print('Detected intent: {} (confidence: {})\n'.format(response.query_result.intent.display_name, response.query_result.intent_detection_confidence))
         print('Fulfillment text: {}\n'.format(response.query_result.fulfillment_text))
-
+        
 
 if __name__ == '__main__':
     file = open('input.txt','r')
     query_text = file.readlines()
     #query_text = ["The system prints the receipt.","The  system ejects  the  ATM  card."]
     detect_intent_texts('seungwoojeong-5b90a',str(uuid.uuid4()),query_text,'en-US')
-
 
